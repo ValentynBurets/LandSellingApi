9fa55d75ff95906c2925990dd22af01e8435b95c
vab <vab@interlogic.com.ua>
2022-02-14 14:21:09 +0200

    migrations

--- LandSellingApi/Data/Migrations/20220214115518_FinalDBUpadatePriceCoefMigration.Designer.cs
+++ LandSellingApi/Data/Migrations/20220214115518_FinalDBUpadatePriceCoefMigration.Designer.cs
@@ -4,15 +4,17 @@ using Data.EF;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using NetTopologySuite.Geometries;
 
 namespace Data.Migrations
 {
     [DbContext(typeof(LandSellingContext))]
-    partial class LandSellingContextModelSnapshot : ModelSnapshot
+    [Migration("20220214115518_FinalDBUpadatePriceCoefMigration")]
+    partial class FinalDBUpadatePriceCoefMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
