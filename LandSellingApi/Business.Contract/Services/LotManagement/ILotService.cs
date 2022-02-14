f6a36d6d8fab6ede7111b9aa525b20a2456ca44c
vab <vab@interlogic.com.ua>
2022-02-14 23:29:45 +0200

    created interface for lot service

--- LandSellingApi/Business.Contract/Services/LotManagement/ILotService.cs
+++ LandSellingApi/Business.Contract/Services/LotManagement/ILotService.cs
@@ -1,8 +1,5 @@
 ﻿using Business.Contract.Model.LotManagement.Lot;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Business.Contract.Services.LotManagement
