70198ea91b23b39e066838f39bc6f3fe54e72a15
vab <vab@interlogic.com.ua>
2022-02-14 14:19:56 +0200

    crated update and crate DTO for lot

---
+++ LandSellingApi/Business.Contract/Model/LotManagement/Lot/CreateLotDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contract.Model.LotManagement.Lot
+{
+    public class CreateLotDTO
+    {
+        public Guid? OwnerId { get; set; }
+        public Guid ManagerId { get; set; }
+        public DateTime PublicationDate { get; set; }
+        public string Description { get; set; }
+        public decimal? MinPrice { get; set; }
+        public decimal? MaxPrice { get; set; }
+        public Geometry Location { get; set; }
+    }
+}
