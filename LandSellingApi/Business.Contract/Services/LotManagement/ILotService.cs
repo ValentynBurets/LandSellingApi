ab218f9f144f15e210d2fb2a8f80467bb23ec608
vab <vab@interlogic.com.ua>
2022-02-14 14:20:17 +0200

    created interface for lot service

---
+++ LandSellingApi/Business.Contract/Services/LotManagement/ILotService.cs
@@ -0,0 +1,16 @@
+﻿using Business.Contract.Model.LotManagement.Lot;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Contract.Services.LotManagement
+{
+    public interface ILotService  
+    {
+        public Task Create(CreateLotDTO createLot);
+        public Task Delete(Guid id);
+        public Task Update(UpdateLotDTO updateLot, Guid lotId);
+    }
+}
