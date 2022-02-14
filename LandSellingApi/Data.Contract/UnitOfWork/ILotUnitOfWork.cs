bb1f561db391cb4342cae86f3cb87fcb2022901c
vab <vab@interlogic.com.ua>
2022-02-14 23:29:09 +0200

    complited interface for unit of work

--- LandSellingApi/Data.Contract/UnitOfWork/ILotUnitOfWork.cs
+++ LandSellingApi/Data.Contract/UnitOfWork/ILotUnitOfWork.cs
@@ -1,8 +1,21 @@
-﻿using Data.Contract.UnitOfWork.Base;
+﻿using Data.Contract.Repository;
+using Data.Contract.Repository.Authentication;
+using Data.Contract.Repository.LotManagement;
+using Data.Contract.UnitOfWork.Base;
 
 namespace Data.Contract.UnitOfWork
 {
     public interface ILotUnitOfWork : IUnitOfWorkBase
     {
+        ILotRepository LotRepository { get; }
+        IRealEstateRepository RealEstateRepository { get; }
+        IRentRepository RentRepository { get; }
+        ISellingRepository SellingRepository { get; }
+        IBidRepository BidRepository { get; }
+        IFavoriteRepository FavoriteRepository {  get; }
+        IImageRepository ImageRepository { get; }
+        IPriceCoefRepository PriceCoefRepository { get; }
+        ICustomerRepository CustomerRepository { get;  }
+        IManagerRepository ManagerRepository { get; }
     }
 }
