601d8afc321d0e135a4816bd819bdeb143adb0e2
valentyn.burets <Valentyn.Burets@gmail.com>
2022-06-15 16:35:13 +0300

    updated lot dto

--- Business.Contract/Model/LotManagement/Lot/UpdateLotDTO.cs
+++ Business.Contract/Model/LotManagement/Lot/UpdateLotDTO.cs
@@ -11,6 +11,8 @@ namespace Business.Contract.Model.LotManagement
         public string Description { get; set; }
         public decimal BuyPrice { get; set; }
         public decimal? MinBidPrice { get; set; }
+        public decimal MinBidStep { get; set; }
+        public int AuctionDuration { get; set; }
         public bool IsRent { get; set; }
         public bool IsAuction { get; set; }
         public LocationDTO Location { get; set; }
